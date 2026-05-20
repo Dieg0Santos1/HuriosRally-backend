b19d02e79ba74d1bfd70cd7c8aa1cf6bb8a14c35
Benchi2312 <u22228397@utp.edu.pe>
2026-05-20 02:47:12 -0500

    feat(user): add profile image upload dependencies

--- src/main/java/com/hurios/huriosbackend/controller/UserController.java
+++ src/main/java/com/hurios/huriosbackend/controller/UserController.java
@@ -10,6 +10,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = "*")
