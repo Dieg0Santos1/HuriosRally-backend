ef779edbd2adb93c409c0df61c335801c0a16acc
Benchi2312 <u22228397@utp.edu.pe>
2026-05-20 03:17:32 -0500

    feat(controller): create ImageUploadController structure

---
+++ src/main/java/com/hurios/huriosbackend/controller/ImageUploadController.java
@@ -0,0 +1,10 @@
+package com.hurios.huriosbackend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/images")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ImageUploadController {
+
+}
